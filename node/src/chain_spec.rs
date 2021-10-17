f50641fb74b4a3a02c75839287057abd89a66962
Unique One <god@unique.one>
2021-10-17 11:37:53 +0800

    Fix

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -4,7 +4,7 @@ use uniqueone_appchain_runtime::{
 	SystemConfig, WASM_BINARY,
 };
 use sc_service::{ChainType, Properties};
-use sp_core::{sr25519, Pair, Public};
+use sp_core::{crypto::UncheckedInto, sr25519, Pair, Public};
 use sp_finality_grandpa::AuthorityId as GrandpaId;
 use sp_runtime::traits::{IdentifyAccount, Verify};
 
